bee2ca979f74d3e372f1ec3a0e5f82092abb5376
LAPTOP-1206GM4S\yonay <yonaycl@gmail.com>
2017-10-28 14:11:43 +0100

    green: have_to_increment_by_2_quality_item_Backstage_when_sellIn_is_10_days_or_less

--- gildedRoseKata/GildedRoseShould.cs
+++ gildedRoseKata/GildedRoseShould.cs
@@ -9,11 +9,16 @@ namespace gildedRoseKata{
         private Item item;
         private List<Item> items;
         private GildedRose gildedRose;
+        private string bactstage = "Backstage passes to a TAFKAL80ETC concert";
+        private string sulfuras = "Sulfuras, Hand of Ragnaros";
+        private string agedBrie = "Aged Brie";
+        private string chair= "chair";
 
         [SetUp]
         public void SetUp(){
             items = new List<Item>();
             gildedRose = new GildedRose(items);
+
         }
 
         private void CreateItem(string name, int sellIn, int quality){
@@ -22,7 +27,7 @@ namespace gildedRoseKata{
 
         [Test]
         public void when_sell_expired_quality_degrades_twice_as_fast(){
-            CreateItem(name:"chair",sellIn:-1,quality: 4);
+            CreateItem(name:chair,sellIn:-1,quality: 4);
             items.Add(item);
             
             gildedRose.UpdateQuality();
@@ -32,7 +37,7 @@ namespace gildedRoseKata{
         
         [Test]
         public void have_not_a_negative_quality(){
-            CreateItem(name:"chair",sellIn:-1,quality: 0);
+            CreateItem(name:chair,sellIn:-1,quality: 0);
             items.Add(item);
             
             gildedRose.UpdateQuality();
@@ -42,7 +47,7 @@ namespace gildedRoseKata{
         
         [Test]
         public void increases_quality_item_aged_brie_when_decrement_sellin(){
-            CreateItem(name:"Aged Brie", sellIn:10, quality:4);
+            CreateItem(name:agedBrie, sellIn:10, quality:4);
             items.Add(item);
             
             gildedRose.UpdateQuality();
@@ -52,7 +57,7 @@ namespace gildedRoseKata{
         
         [Test]
         public void have_not_quality_item_more_than_50(){
-            CreateItem(name:"Aged Brie", sellIn:10, quality:50);
+            CreateItem(name:agedBrie, sellIn:10, quality:50);
             items.Add(item);
             
             gildedRose.UpdateQuality();
@@ -62,12 +67,22 @@ namespace gildedRoseKata{
         
         [Test]
         public void have_not_drecreases_in_quality_item_sulfuras(){
-            CreateItem(name:"Sulfuras, Hand of Ragnaros", sellIn:10, quality:80);
+            CreateItem(name:sulfuras, sellIn:10, quality:80);
             items.Add(item);
             
             gildedRose.UpdateQuality();
             
             Assert.AreEqual(80,item.Quality);
+        }
+        
+        [Test]
+        public void have_to_increment_by_2_quality_item_Backstage_when_sellIn_is_10_days_or_less(){
+            CreateItem(name:bactstage, sellIn:10, quality:40);
+            items.Add(item);
+            
+            gildedRose.UpdateQuality();
+            
+            Assert.AreEqual(42,item.Quality);
         }
         
     }
